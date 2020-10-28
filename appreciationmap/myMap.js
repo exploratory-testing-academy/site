a5e322e7d2c190da2fbb765ed5fd26ae07a718a7
maaretp <maaret@iki.fi>
2020-10-28 22:53:17 +0200

    Bigger icon

--- appreciationmap/myMap.js
+++ appreciationmap/myMap.js
@@ -8,7 +8,7 @@ function myMap() {
     var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
     var marker = new google.maps.Marker({
         position: helsinkiFinland,
-        icon:'../ExploratoryTesting15x.png'
+        icon:'../ExploratoryTesting30x.png'
         });
     
     marker.setMap(map);
