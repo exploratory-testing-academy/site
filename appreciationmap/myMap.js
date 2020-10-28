2687763024f4f701a82b54b5d80f48c190b721bd
maaretp <maaret@iki.fi>
2020-10-28 22:44:32 +0200

    Adding appreciationmap starter

---
+++ appreciationmap/myMap.js
@@ -0,0 +1,27 @@
+
+function myMap() {
+    const helsinkiFinland = { lat: 60.192059, lng: 24.945831 };
+    var mapProp= {
+      center: helsinkiFinland, 
+      zoom:2,
+    };
+    var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+    var marker = new google.maps.Marker({
+        position: helsinkiFinland,
+        icon:'../ExploratoryTesting30x.png'
+        });
+    
+    marker.setMap(map);
+
+    var infowindow = new google.maps.InfoWindow({
+        content:"Maaret Pyhäjärvi"
+      });
+      
+      google.maps.event.addListener(marker, 'click', function() {
+        infowindow.open(map,marker);
+      });
+
+    }
+
+
+
