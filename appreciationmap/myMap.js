4af5afa3a6efaedb79620b775090206f1648b464
maaretp <maaret@iki.fi>
2021-01-03 14:06:52 +0200

    Updated map

--- appreciationmap/myMap.js
+++ appreciationmap/myMap.js
@@ -5,47 +5,64 @@ function myMap() {
     const rotterdamNetherlands = { lat: 51.905445, lng: 4.466637}
     const sydneyAustralia = {lat: -33.865143, lng: 151.209900}
     const oregonUSA = {lat: 43.804134, lng: -120.554199}
-    const somewhereGermany = {lat: 51.165691, lng: 10.451526} 
+    const somewhereGermany = {lat: 51.165691, lng: 10.451526}
+    const somewhereUK = {lat: 51.509865, lng: -0.118092}
+    const somewhereCroatia = {lat: 43.508133, lng: 16.440193}
     var mapProp= {
-      center: betterCenter, 
+      center: betterCenter,
       zoom:2,
     };
     var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-    
+
     var markerMaaret = new google.maps.Marker({
         position: helsinkiFinland,
         icon:'img/pin20x.png'
         });
 
     var markerMirja = new google.maps.Marker({
-        position: helsinkiFinland, 
+        position: helsinkiFinland,
         icon:'img/pin20x.png'
         });
 
     var markerRu = new google.maps.Marker({
-        position: helsinkiFinland, 
+        position: helsinkiFinland,
         icon:'img/pin20x.png'
         });
 
     var markerElizabeth = new google.maps.Marker({
-      position: rotterdamNetherlands, 
+      position: rotterdamNetherlands,
       icon:'img/pin20x.png'
         });
 
     var markerAnneMarie = new google.maps.Marker({
-      position: sydneyAustralia, 
+      position: sydneyAustralia,
       icon:'img/pin20x.png'
         });
 
     var markerCarol = new google.maps.Marker({
-      position: oregonUSA, 
+      position: oregonUSA,
       icon:'img/pin20x.png'
-        });        
+        });
 
     var markerAlex = new google.maps.Marker({
-      position: somewhereGermany, 
+      position: somewhereGermany,
       icon:'img/pin20x.png'
-        }); 
+        });
+
+    var markerAngela = new google.maps.Marker({
+      position: oregonUSA,
+      icon:'img/pin20x.png'
+        });
+
+    var markerParveen = new google.maps.Marker({
+      position: somewhereUK,
+      icon:'img/pin20x.png'
+        });
+
+    var markerIrja = new google.maps.Marker({
+      position: somewhereCroatia,
+      icon:'img/pin20x.png'
+        });
 
     markerMaaret.setMap(map);
     markerMirja.setMap(map);
@@ -54,11 +71,14 @@ function myMap() {
     markerCarol.setMap(map);
     markerAlex.setMap(map);
     markerRu.setMap(map);
+    markerAngela.setMap(map);
+    markerParveen.setMap(map);
+    markerIrja.setMap(map);
 
     var infowindowMaaret = new google.maps.InfoWindow({
         content:"Maaret Pyhäjärvi"
       });
-      
+
     /*var infowindowMirja = new google.maps.InfoWindow({
         content:"Mirja Pyhäjärvi"
       });
@@ -75,14 +95,11 @@ function myMap() {
 /*
     google.maps.event.addListener(markerMirja, 'click', function() {
         infowindowMirja.open(map,markerMirja);
-      }); 
-    
+      });
+
     google.maps.event.addListener(markerElizabeth, 'click', function() {
         infowindowElizabeth.open(map,markerElizabeth);
       });
       */
 
     }
-
-
-
